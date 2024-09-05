765bd7eae8f44915a1a046a7da06947e22c33ca9
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-05 12:08:19 -0400

    Implemented auth routes and controllers

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send('Welcome signup');
+};
+
+export const login = async (req, res) => {
+    res.send('Welcome login');
+};
+
+export const logout = async (req, res) => {
+    res.send('Welcome logout');
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+// Import routes
+import authRoutes from './routes/authRoutes.js';
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5002;
 
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => [
     console.log(`Server is running on port ${PORT}`)
