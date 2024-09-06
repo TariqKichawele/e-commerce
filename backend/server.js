b87d7a87ffac79440f7fb3d37d381176c40392bd
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-06 19:07:26 -0400

    Implemented analytics route and controllers functionality

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import productsRoutes from './routes/productsRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
 import couponRoutes from './routes/couponRoutes.js';
 import paymentRoutes from './routes/paymentRoutes.js';
+import analyticsRoutes from './routes/analyticsRoutes.js';
 import { connectDB } from './lib/db.js';
 
 dotenv.config();
@@ -23,6 +24,7 @@ app.use('/api/products', productsRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/coupon', couponRoutes);
 app.use('/api/payments', paymentRoutes);
+app.use('/api/analytics', analyticsRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
