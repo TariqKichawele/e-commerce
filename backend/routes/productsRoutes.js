2d258cae72fdfab9137b76c68967fca19051c91c
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-06 11:10:31 -0400

    Implemented deleteProduct functionality

--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllProducts, getFeaturedProducts, createProduct } from '../controllers/productControllers.js';
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct } from '../controllers/productControllers.js';
 import { adminRoute, protectRoute } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', protectRoute, adminRoute, getAllProducts);
 router.get('/featured', getFeaturedProducts);
 router.post('/', protectRoute, adminRoute, createProduct);
+router.delete('/:id', protectRoute, adminRoute, deleteProduct);
 
 export default router;
