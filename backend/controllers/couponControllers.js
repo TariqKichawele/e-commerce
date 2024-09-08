597206c019433f8601d690f2ee26c2238994e86f
TariqKichawele <tariqkichawele66@gmail.com>
2024-09-08 11:18:47 -0400

    Debugged getCoupon controller

--- backend/controllers/couponControllers.js
+++ backend/controllers/couponControllers.js
@@ -4,10 +4,10 @@ import Coupon from "../models/couponModel.js";
 export const getCoupon = async (req, res) => {
     try {
         const coupon = await Coupon.findOne({
-            userId: req.user.id,
+            userId: req.user._id,
             isActive: true,
         });
-        res.json(coupon | null);
+        res.json(coupon);
     } catch (error) {
         console.log('Error in getCoupon', error.message);
         res.status(500).json({ message: error.message });        
